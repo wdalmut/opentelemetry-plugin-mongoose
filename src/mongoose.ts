9e5933461daf959c2948779d745177c629f660df
Walter Dal Mut <walter.dalmut@gmail.com>
2020-04-05 23:40:01 +0200

    Fixes #8 fixes #12 - Use shimmer wrap instead of pre/post hooks

--- src/mongoose.ts
+++ src/mongoose.ts
@@ -1,10 +1,10 @@
 import { BasePlugin } from '@opentelemetry/core';
-import { CanonicalCode, Span, SpanKind } from '@opentelemetry/api';
 import * as shimmer from 'shimmer';
-import { AttributeNames } from './enums';
-import mongoose, { Schema, Document, Model, Query } from 'mongoose';
-import mongodb from 'mongodb'
-import { middlewares } from './middleware';
+import mongoose, { Schema, Document, Model, Collection } from 'mongoose';
+
+import { AttributeNames } from './enums'
+
+import { startSpan, handleError } from './utils'
 
 import { VERSION } from './version'
 
@@ -18,97 +18,69 @@ export class MongoosePlugin extends BasePlugin<typeof mongoose> {
       this._logger.debug('MongoosePlugin: patch mongoose plugin');
 
       shimmer.wrap(this._moduleExports, 'model', this.patchModel());
+      shimmer.wrap(this._moduleExports.Query.prototype, 'exec', this.patchQueryExec());
       shimmer.wrap(this._moduleExports.Mongoose.prototype, 'model', this.patchModel());
     }
     return this._moduleExports;
   }
 
-  private patchModel() {
-    return (originalModel: Function) => {
-      const thisPlugin = this
-      thisPlugin._logger.debug('MongoosePlugin: patched mongoose model');
+  private patchQueryExec() {
+    const thisPlugin = this
+    thisPlugin._logger.debug('MongoosePlugin: patched mongoose query exec prototype');
+    return (originalExec: Function) => {
+      return function exec(this: any) {
+        let span = startSpan(thisPlugin._tracer, this.model.modelName, this.op);
 
+        span.setAttribute(AttributeNames.DB_NAME, this.mongooseCollection.conn.name)
+        span.setAttribute(AttributeNames.COLLECTION_NAME, this.collection.name)
+
+        span.setAttribute(AttributeNames.DB_QUERY_TYPE, this.op)
+        span.setAttribute(AttributeNames.DB_STATEMENT, JSON.stringify(this._conditions))
+        span.setAttribute(AttributeNames.DB_OPTIONS, JSON.stringify(this.options))
+        span.setAttribute(AttributeNames.DB_UPDATE, JSON.stringify(this._update))
+
+        const queryResponse = originalExec.apply(this, arguments)
+        span.end()
+        return queryResponse
+      }
+    }
+  }
+
+  private patchModel() {
+    const thisPlugin = this
+    thisPlugin._logger.debug('MongoosePlugin: patched mongoose model');
+    return (originalModel: Function) => {
       return function model<T extends Document>(name: string, schema?: Schema, collection?: string, skipInit?: boolean): Model<T> {
         if (!schema) {
           const returned = originalModel(...arguments)
           return returned;
         }
 
-        for (let middleware of middlewares) {
-          schema.pre(middleware, function() {
-            thisPlugin._logger.debug('MongoosePlugin: pre mongoose query');
-
-            let span = thisPlugin._tracer.startSpan(`mongoose.${name}.${middleware}`, {
-              kind: SpanKind.CLIENT,
-              attributes: {
-                // TODO: Add collection and database name and connection information
-                [AttributeNames.DB_MODEL_NAME]: name,
-                [AttributeNames.DB_QUERY_TYPE]: middleware,
-                [AttributeNames.DB_TYPE]: 'nosql',
-                [AttributeNames.COMPONENT]: 'mongoose',
-              },
-            });
-
-            if (this instanceof Query) {
-              span.setAttribute(AttributeNames.DB_STATEMENT, this.getQuery() ? JSON.stringify(this.getQuery()) : null);
-              span.setAttribute(AttributeNames.DB_OPTIONS, this.getOptions() ? JSON.stringify(this.getOptions()) : null);
-              span.setAttribute(AttributeNames.DB_UPDATE, this.getUpdate() ? JSON.stringify(this.getUpdate()) : null);
-            }
-
-            // @ts-ignore
-            this._span = span
-          })
-
-          // handle normal operations
-          schema.post(middleware, function (doc: T, next: Function) {
-            thisPlugin._logger.debug('MongoosePlugin: post mongoose query');
-
-            // @ts-ignore
-            const span: Span = this._span;
-
-            if (!span) {
-              thisPlugin._logger.debug('MongoosePlugin: There is no span in place...');
-              return next();
-            }
-
-            span.setStatus({
-              code: CanonicalCode.OK,
-            });
-
-            span.setAttribute(AttributeNames.DB_MODEL, JSON.stringify(doc));
-
-            span.end();
-
-            return next()
-          })
-
-          // handle error conditions
-          schema.post(middleware, function(error: mongodb.MongoError, doc: T, next: Function) {
-            thisPlugin._logger.debug('MongoosePlugin: post mongoose query');
-
-            // @ts-ignore
-            const span: Span = this._span;
-
-            if (!span) {
-              thisPlugin._logger.debug('MongoosePlugin: There is no span in place...');
-              return next()
-            }
-
-            span.setAttribute(AttributeNames.MONGO_ERROR_CODE, error.code);
-            span.setStatus({
-              code: CanonicalCode.UNKNOWN,
-              message: error.message,
-            })
-
-            // @ts-ignore
-            span.end();
-
-            next()
-          })
-        }
-
         const returned = originalModel(... arguments)
+
+        let m = mongoose.model(name)
+
+        shimmer.wrap(m.prototype, 'save', thisPlugin.patchSave(name, m.collection))
+
         return returned;
+      }
+    }
+  }
+
+  private patchSave(name: string, collection: Collection) {
+    const thisPlugin = this
+    thisPlugin._logger.debug('MongoosePlugin: patched mongoose save prototype');
+    return (originalSave: Function) => {
+      return function save(this: any) {
+        let span = startSpan(thisPlugin._tracer, name, 'save');
+
+        span.setAttribute(AttributeNames.DB_QUERY_TYPE, 'save')
+        span.setAttribute(AttributeNames.DB_NAME, collection.conn.name)
+        span.setAttribute(AttributeNames.COLLECTION_NAME, collection.name)
+
+        return originalSave.apply(this, arguments)
+          .catch(handleError(span))
+          .finally(() => span.end() )
       }
     }
   }
